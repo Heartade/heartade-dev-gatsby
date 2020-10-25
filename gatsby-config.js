eb9a0fcac5c6281c0ac32fc279a277fd9644315b
Heartade <soobka0313@gmail.com>
2020-10-25 09:24:49 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `The Heartade Space`,
+    author: `Daniel S. Park`,
+    description: `사는 게 다 이렇지`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/heartade_`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/Heartade`,
+      },
+    ],
+  },
+}
---
+++ serve.sh
@@ -0,0 +1,4 @@
+cd ./content/posts/
+git pull
+cd ../..
+gatsby build
